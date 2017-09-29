71b2e3f8e4cce726f81fa53ae4f97def07cf7116
无北 <conglaiwubei@163.com>
2017-09-29 23:30:29 +0800

    return logId

--- cloud.js
+++ cloud.js
@@ -1,6 +1,9 @@
 var AV = require('leanengine');
 
 
+// 获取用户名 request.currentUser.get("username")
+// 判断是否登录 if(request.currentUser)
+
 AV.Cloud.define('DiveLog.GetGroupId', function(request) {
-  return { "GroupId": request.currentUser.get("username")};
+  return { "GroupId": request.body.LogId};
 });
